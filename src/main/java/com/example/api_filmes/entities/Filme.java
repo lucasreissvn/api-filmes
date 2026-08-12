0f5e5c5fdc96e1e4617b0d54f99b3ab34c1b5573
lucasreiss <lucasreissilvino@gmail.com>
2026-08-11 21:04:47 -0300

    feat: cria entidade filme

---
+++ src/main/java/com/example/api_filmes/entities/Filme.java
@@ -0,0 +1,85 @@
+package com.example.api_filmes.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_filme")
+public class Filme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Integer duracao;
+    private String genero;
+    private LocalDate dataLancamento;
+    private Double nota;
+
+    public Filme(){}
+
+    public Filme(Long id, String nome, Integer duracao, String genero, LocalDate dataLancamento, Double nota) {
+        this.id = id;
+        this.nome = nome;
+        this.duracao = duracao;
+        this.genero = genero;
+        this.dataLancamento = dataLancamento;
+        this.nota = nota;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(Integer duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+    
+
+}
