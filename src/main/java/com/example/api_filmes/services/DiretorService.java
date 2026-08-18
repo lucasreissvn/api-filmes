5a2749ddf94da68cebf69d634b6cd01c181e0b90
lucasreissvn <lucasreissilvino@gmail.com>
2026-08-18 11:16:03 -0300

    adiciona diretor service

---
+++ src/main/java/com/example/api_filmes/services/DiretorService.java
@@ -0,0 +1,47 @@
+package com.example.api_filmes.services;
+
+import org.springframework.stereotype.Service;
+
+import com.example.api_filmes.dto.DiretorDTO;
+import com.example.api_filmes.entities.Diretor;
+import com.example.api_filmes.exceptions.DiretorNaoEncontradoException;
+import com.example.api_filmes.repositories.DiretorRepository;
+
+@Service
+public class DiretorService{
+    private final DiretorRepository diretorRepository;
+
+    public DiretorService(DiretorRepository diretorRepository) {
+        this.diretorRepository = diretorRepository;
+    }
+
+    public Diretor buscarPorId(Long id){
+        return diretorRepository.findById(id)
+        .orElseThrow(() -> new DiretorNaoEncontradoException(id));
+    }
+
+    public Diretor salvarDiretor(DiretorDTO diretorDTO){
+        Diretor diretor = new Diretor();
+
+        diretor.setNome(diretorDTO.nome());
+        diretor.setDataNascimento(diretorDTO.dataNascimento());
+
+        return diretorRepository.save(diretor);
+
+    }
+
+    public void deletarDiretor(Long id){
+        if(!diretorRepository.existsById(id)){
+            throw new DiretorNaoEncontradoException(id);
+        }
+        diretorRepository.deleteById(id);
+    }
+
+    public Diretor atualizarDiretor(Long id, DiretorDTO diretorDTO){
+        Diretor diretorExistente = diretorRepository.findById(id).orElseThrow(() -> new DiretorNaoEncontradoException(id));
+
+        diretorExistente.setNome(diretorDTO.nome());
+        diretorExistente.setDataNascimento(diretorDTO.dataNascimento());
+        return diretorRepository.save(diretorExistente);
+    }
+}
